dd6dd6dbb14eae3a23857bc97727bc95ca8c15f2
Matthew Weihl <mattdweihl@gmail.com>
2021-10-15 17:36:21 -0400

    Update bashrc, add PowerShell profile

--- .bashrc
+++ .bashrc
@@ -12,8 +12,11 @@ export LS_COLORS='ow=01;36;40'
 
 # Source PS1 variable
 function nonzero_return() {
-	RETVAL=$?
-	[ $RETVAL -ne 0 ] && echo "$RETVAL"
+    RETVAL=$?
+    [ $RETVAL -ne 0 ] && echo "$RETVAL"
 }
 
 export PS1="\[\e[31m\]\`nonzero_return\`\[\e[m\][\[\e[35m\]\@\[\e[m\]] \[\e[32m\]\u\[\e[m\]@\[\e[33m\]\h\[\e[m\]:\[\e[34m\]\W\[\e[m\]\\n↳ "
+#PROMPT_COMMAND='echo -en "\033]0; $("pwd") \a"'
+PROMPT_COMMAND='echo -en "\033]0;$(whoami)@$(hostname)|$(pwd|cut -d "/" -f 4-100)\a"'
+
---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,38 @@
+function get_time()
+{
+	$ESC = [char]27
+	return "$ESC[35m$(Get-Date -UFormat "%I:%M %p")$ESC[0m"
+}
+
+function get_username()
+{
+	$ESC = [char]27
+	return "$ESC[32m$env:UserName$ESC[0m"
+}
+
+function get_hostname()
+{
+	$ESC = [char]27
+	return "$ESC[33m$(hostname)$ESC[0m"
+}
+
+function get_location()
+{
+	$ESC = [char]27
+    return "$(pwd | Convert-Path)"
+}
+
+function prompt {
+	$Host.UI.RawUI.WindowTitle = "$pwd"
+	return "[$(get_time)] $(get_username)@$(get_hostname):$(get_location) `n↳ "
+}
+
+function Get-ChildItemUnix {
+    Get-ChildItem $Args[0] |
+        Format-Table Mode, @{N='Owner';E={(Get-Acl $_.FullName).Owner}}, Length, LastWriteTime, @{N='Name';E={if($_.Target) {$_.Name+' -> '+$_.Target} else {$_.Name}}}
+}
+
+if (!(test-path alias:ll))
+{
+	New-Alias ll Get-ChildItemUnix
+}
