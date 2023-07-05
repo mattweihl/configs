d671fdf3fec167960a52035b1bbbdbaba690430b
Matthew Weihl <mattdweihl@gmail.com>
2023-07-05 04:22:57 -0400

    update ripgrep and telescope neovim config

--- nvim/config.lua
+++ nvim/config.lua
@@ -1,10 +1,26 @@
-require('telescope').setup{ 
-  defaults = { 
-    file_ignore_patterns = { 
-      "node_modules", 
-      "node-offline-mirror",
-      "node-packages",
-      ".git"
-    }
-  }
+require('telescope').setup{
+  defaults = {
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case'
+    },
+    file_ignore_patterns = {
+      "node_modules/*",
+      "node-offline-mirror/*",
+      "node-packages/*",
+    },
+  },
+  pickers = {
+    find_files = {
+      find_command = {
+        'rg', '--files', '--hidden', '--no-ignore', '--glob=!node_modules/*', '--glob=!node-offline-mirror/*', '--glob=!node-packages/*'
+      }
+    },
+  },
 }
+
