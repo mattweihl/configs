9c9fc45ae513be23de29e7d77a60aed785707341
Matthew Weihl <mattdweihl@gmail.com>
2026-02-08 11:57:45 -0500

    update

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,4 +1,14 @@
 return {
+  -- Lazygit in a floating terminal
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = "LazyGit",
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+
   {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
